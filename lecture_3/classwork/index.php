8b057fc2efca1859cc85af0018674e9e1602a7f9
LAB321 Student02 <lab321student02@gau.local>
2026-03-04 11:43:04 +0400

    classwork

---
+++ lecture_3/classwork/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        form {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+            width: 800px;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            border: 1px solid black;
+        }
+        #rame{
+            background-color: red;
+        }
+        .empty{
+            width: 0px;
+
+        }
+    </style>
+</head>
+<body>
+    <form action="">
+        
+        <table>
+            <tr>
+                <td>Question</td>
+                <td>Answers</td>
+                <td>Max Point</td>
+            </tr>
+            <tr>
+                <td>what is JS?</td>
+                <td><input type="text"></td>
+                <td>8</td>
+            </tr>
+            <tr>
+                <td>what is react?</td>
+                <td><input type="text"></td>
+                <td>9</td>
+            </tr>
+            <tr>
+                <td>what is angular</td>
+                <td><input type="text"></td>
+                <td>10</td>
+            </tr>
+            <tr>
+                <td>what is html</td>
+                <td><input type="text"></td>
+                <td>8</td>
+            </tr>
+            <tr>
+                <td>what is PHP</td>
+                <td><input type="text"></td>
+                <td>10</td>
+            </tr>
+            <tr>
+                <td>what is css</td>
+                <td><input type="text"></td>
+                <td>10</td>
+            </tr>
+            <tr id="rame">
+                <td>student <input type="text"> <input type="text"> <button> send</button></td>
+                
+            </tr>
+            <tr class="empty"></tr>
+            <tr class="empty"></tr>
+
+
+        </table>
+
+    </form>
+</body>
+</html>
